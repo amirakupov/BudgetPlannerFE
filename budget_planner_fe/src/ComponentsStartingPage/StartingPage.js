f694edafb8a2341c612d8159d6df2f08a9869f84
Jeeems10 <jamesponce10@outlook.com>
2024-01-20 18:47:23 +0100

    landingPage quasi done

--- budget_planner_fe/src/ComponentsStartingPage/StartingPage.js
+++ budget_planner_fe/src/ComponentsStartingPage/StartingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import {BudgeText} from './BudgeText'
 
 const StartingPage = () => {
   return (
     <div>
       <Navbar/>
+      <BudgeText/>
       
     </div>
   )
